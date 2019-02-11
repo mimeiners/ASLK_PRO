dfb1fcdbed2672d8b6cfeda4b35c098395a579a1
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2019-02-11 08:58:54 +0100

    Mathe überarbeitet.

--- docs/math/Experiment_01.m
+++ docs/math/Experiment_01.m
@@ -3,24 +3,28 @@ close all
 clc
 
 %% Parameter für die Übertragungsfunktion 
-%(w1 und w2 können beim open loop gain vernachlässigt werden)
-A0 = 100000;
+% (w1 und w2 können beim open-loop gain vernachlässigt werden)
+% Ja, das stimmt, aber hier geht es um closed-loop.
+A0 = 1e5;
 f_un = 3e6;
 f_3_db = f_un/A0;
-w1 = f_3_db*pi*2;
-w2 = f_un*pi*2;
-GB = A0*w1;
-w0 = 3e6*pi*2;
+wd1 = 2*pi*f_3_db;
+wd2 = 2*pi*4e6;
+GB = A0*wd1;
+w0 = sqrt(GB*wd2);
+
 %% Formel für den Güte Faktor 
-Q = 1/((sqrt(w2/GB)) + ((1/A0)*(sqrt(GB/w2))));
+Q = 1/((sqrt(wd2/GB)) + ((1/A0)*(sqrt(GB/wd2))));
 
 %% Zähler und Nennerpolynom der Übertragungsfunktion
 num = 1;
-den = [1, 1/(w1*Q), 1/((w1)^2)];
-%% Plot der Ergebnisse
-f = linspace(0,4e6,1001);
-w = 2*pi*f;
+%% Zaehler war falsch sortiert und Zeilenvektor.
+den = [1/wd1^2 1/(w0*Q) 1];
+%        s^2      s     1 
 
+%% Plot der Ergebnisse
+f = logspace(1,6);
+w = 2*pi*f;
 freqs(num, den, w);
 
 title('magnitude and phase response of an open loop amplifier')
