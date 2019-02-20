cc42a9718b81505707639e4a961025f720f66cd8
Selim Cimen <secimen@stud.hs-bremen.de>
2019-02-20 19:07:29 +0100

    Upload

---
+++ docs/math/Experiment_02/Simulation 2.2.3/astabile_kippstufe.m
@@ -0,0 +1,28 @@
+clear all 
+close all
+clc
+
+%% Einlesen der Rohdaten
+
+a_kipp = fopen('astabile_kippstufe.txt','r');
+heading_inv_R500 = textscan(a_kipp,'%s %s %s',1);
+fgetl(a_kipp);
+data_1 = textscan(a_kipp,'%n %n %n');
+fclose(a_kipp); 
+
+%% Anlegen der Matrizen
+time = cell2mat(data_1(1)).*1e3;
+Vo = cell2mat(data_1(2));
+Vc = cell2mat(data_1(3));
+
+%% Plot der Ergebnisse
+
+figure(1)
+hold all
+plot(time, Vo)
+plot(time,Vc)
+title('Astabile Kippstufe')
+xlabel('t in ms')
+ylabel('Amplitude in V')
+legend('Spannung V_c','Spannung V_o')
+grid;
