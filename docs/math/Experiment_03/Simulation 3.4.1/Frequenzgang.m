b341b632ca373e28646cd6f28808f2d49bbbfbbc
Selim Cimen <secimen@stud.hs-bremen.de>
2019-03-14 13:21:18 +0100

    up

--- docs/math/Experiment_03/Simulation 3.4.1/Frequenzgang.m
+++ docs/math/Experiment_03/Simulation 3.4.1/Frequenzgang.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-%% Einlesen der Rohdaten
+%% Einlesen der Simulationsdaten
 inte = fopen('Response_Integrator.txt','r');
 heading_in = textscan(inte,'%s %s %s',1);
 fgetl(inte);
@@ -13,7 +13,16 @@ diff = fopen('Response_Differentiator.txt','r');
 heading_diff = textscan(diff,'%s %s %s',1);
 fgetl(diff);
 data2 = textscan(diff,'%n %n %n');
-fclose(diff); 
+fclose(diff);
+
+%% Einlesen der Messdaten
+diff_1 = fopen('diff.txt', 'r');
+data3 = textscan(diff_1, ' %n %n %n');
+fclose(diff_1);
+
+integrator = fopen('integrator.txt','r');
+data4 = textscan(integrator, '%n %n %n');
+fclose(integrator);
 
 %% anlegen der Vektoren
 Freq_in = cell2mat(data1(1));
@@ -24,35 +33,38 @@ Freq_diff = cell2mat(data2(1));
 ampl_diff = cell2mat(data2(2));
 phase_diff = cell2mat(data2(3));
 
+fre_diff_m = cell2mat(data3(1));
+ampl_diff_m = cell2mat(data3(3));
+ampl_diff_m = medfilt1(ampl_diff_m, 3);
+
+fre_int = cell2mat(data4(1));
+ampl_int = cell2mat(data4(3));
+ampl_int = medfilt1(ampl_int, 3);
+
 %% plot der Simulationen
 figure(1)
-hold all
 subplot(211)
-semilogx(Freq_in, ampl_in)
+hold all
+p_1 = plot(fre_int, ampl_int, ':');
+p_2 = plot(Freq_in, ampl_in, '--');
 title('Amplitudengang Integrator')
+legend('Messung','Simulation')
 xlabel('Frequenz in MHz')
-ylabel('Amplitude in V')
+ylabel('Verstärkung in db')
+set(gca, 'XScale', 'log')
+p_1.LineWidth = 2;
+p_2.LineWidth = 2;
 grid;
 subplot(212)
-semilogx(Freq_in, phase_in)
-title('Phasengang Integrator')
-xlabel('Frequenz in MHz')
-ylabel('Phase in degree')
-ylim([-10 100])
-grid;
-
-figure(2)
 hold all
-hold all
-subplot(211)
-semilogx(Freq_diff, ampl_diff)
+p_1 = plot(fre_diff_m, ampl_diff_m, ':');
+p_2 = plot(Freq_diff, ampl_diff, '--');
 title('Amplitudengang Differentiator')
 xlabel('Frequenz in MHz')
-ylabel('Amplitude in V')
+ylabel('Verstärkung in db')
+legend('Messung','Simulation')
+set(gca, 'XScale', 'log')
+p_1.LineWidth = 2;
+p_2.LineWidth = 2;
 grid;
-subplot(212)
-semilogx(Freq_diff, phase_diff)
-title('Phasengang Differentiator')
-xlabel('Frequenz in MHz')
-ylabel('Phase in degree')
-grid;
+
